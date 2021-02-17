4c58c1b5c52c43df1861e2ffcd5d44b1cd0a9300
darwin808 <darwinnn.darwinnn@gmail.com>
2021-02-17 10:34:03 +0800

    asdadsa

---
+++ src/Reducers/AllReducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import PeopleReducer from "./PeopleReducer";
+import CounterReducer from "./CounterReducer";
+
+const AllReducers = combineReducers({ PeopleReducer, CounterReducer });
+export default AllReducers;
